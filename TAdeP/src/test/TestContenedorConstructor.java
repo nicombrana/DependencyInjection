7076864d219118b56b20447152577afb58c75872
Nico Braná <nicolasmbrana@gmail.com>
2013-06-06 23:55:02 -0300

    Me estoy fijando lo de los constructores, quiero que los test no me rompan, pero que fallen hasta que vea como hacer los constructores.

--- TAdeP/src/test/TestContenedorConstructor.java
+++ TAdeP/src/test/TestContenedorConstructor.java
@@ -9,19 +9,54 @@ import src.ContenedorConstructor;
 
 public class TestContenedorConstructor {
 	
-	private ContenedorConstructor contCons;
+	private ContenedorConstructor contConstructor;
 	
 	@Before
 	public void setUp() throws Exception {
-		contCons = new ContenedorConstructor();
-		contCons.configurate(PersonaHome.class, PersonaDataBaseMock.class);
-		contCons.configurate(PersonaService.class, PersonaService.class);
-		contCons.agregarDependencia(PersonaHome.class);
+		contConstructor = new ContenedorConstructor();
+		contConstructor.configurate(PersonaDataBaseHome.class, PersonaDataBaseMock.class);
+		contConstructor.configurate(PersonaService.class, PersonaService.class);
+		contConstructor.configurate("Usuario",String.class,"Franco");
+		contConstructor.configurate("Password",String.class,"VamosQueFunca!");
+		contConstructor.configurate("Host",String.class,"ElHost");
+		contConstructor.configurate("Puerto",int.class,1234);
+		contConstructor.configurate("Esquema",String.class,"Blah");
+		contConstructor.agregarDependencia(PersonaService.class,PersonaDataBaseHome.class);
+		contConstructor.agregarDependencia(PersonaDataBaseHome.class,"Usuario");
+		contConstructor.agregarDependencia(PersonaDataBaseHome.class,"Password");
+		contConstructor.agregarDependencia(PersonaDataBaseHome.class,"Host");
+		contConstructor.agregarDependencia(PersonaDataBaseHome.class,"Puerto");
+		contConstructor.agregarDependencia(PersonaDataBaseHome.class,"Esquema");
 	}
 	
 	@Test
 	public void testContenedorConstructor() throws Exception {
-		PersonaService personita = (PersonaService) contCons.dameUnObjeto(PersonaService.class);
+		PersonaService personita = (PersonaService) contConstructor.dameUnObjeto(PersonaService.class);
 		Assert.assertEquals(personita.getPersonaHome().getUsuario(), "Franco");
+	}
+	
+	@Test
+	public void testPersonaServiceTieneUnPersonaDataBaseMock() throws Exception {
+		PersonaService personaService = (PersonaService) contConstructor.dameUnObjeto(PersonaService.class);
+		Assert.assertEquals(PersonaDataBaseMock.class, personaService.getPersonaHome().getClass());
+	}
+	
+	@Test
+	public void testContenedorObjetos() throws Exception {
+		contConstructor = new ContenedorConstructor();
+		contConstructor.configurate(PersonaDataBaseHome.class, PersonaDataBaseHome.class);
+		contConstructor.configurate("Usuario",String.class,"Fede");
+		contConstructor.configurate("Password",String.class,"AguanteEsteGrupo");
+		contConstructor.configurate("Host",String.class,"ElHost");
+		contConstructor.configurate("Puerto",int.class,1234);
+		contConstructor.configurate("Esquema",String.class,"Blah");
+		contConstructor.agregarDependencia(PersonaDataBaseHome.class,"Usuario");
+		contConstructor.agregarDependencia(PersonaDataBaseHome.class,"Password");
+		contConstructor.agregarDependencia(PersonaDataBaseHome.class,"Host");
+		contConstructor.agregarDependencia(PersonaDataBaseHome.class,"Puerto");
+		contConstructor.agregarDependencia(PersonaDataBaseHome.class,"Esquema");
+	
+		PersonaDataBaseHome persoDB = (PersonaDataBaseHome) contConstructor.dameUnObjeto(PersonaDataBaseHome.class);
+		Assert.assertEquals("Fede", persoDB.getUsuario());
 	}
 }
